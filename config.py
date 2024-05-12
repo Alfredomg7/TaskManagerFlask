815d1ceea5563c64cc1af107f930c6707767154a
Alfredomg7 <alfredomg4000@gmail.com>
2024-05-12 16:55:06 -0700

    Update config module to follow best security practices

--- config.py
+++ config.py
@@ -4,16 +4,16 @@ from datetime import timedelta
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'ltUN7a9JZcrzQWFmKtU_QA')
-    SECURITY_PASSWORD_SALT = os.environ.get('SECURITY_PASSWORD_SALT', 'P+bAN55/87EJ59Y3jS6r+Rnhy6lAxol1yo/hB0GeHfw=')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECURITY_PASSWORD_SALT = os.environ.get('SECURITY_PASSWORD_SALT')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, '../instance/test.db')
     REMEMBER_COOKIE_DURATION = timedelta(days=7)
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.example.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') is None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'your-email@example.com')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'your_password')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'True').lower() in ['true', '1', 't']
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'False').lower() in ['true', '1', 't']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 class DevelopmentConfig(Config):
     DEBUG = True
